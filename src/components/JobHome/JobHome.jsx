779a9eadc16c09bc50e96f24d9afaded0c84de1c
Aditta Chakraborty <adittacse@gmail.com>
2023-04-10 11:51:20 +0600

    JobHome layout updated with separated components

--- src/components/JobHome/JobHome.jsx
+++ src/components/JobHome/JobHome.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import "./JobHome.css";
-import person from "../../../assets/All Images/person.png"
 import JobCategory from "../JobCategory/JobCategory.jsx";
 import Banner from "../Banner/Banner.jsx";
 
