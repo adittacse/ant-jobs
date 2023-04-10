eaf9a0057c50bfd82ff0e57653a22b4801b940fd
Aditta Chakraborty <adittacse@gmail.com>
2023-04-10 11:30:58 +0600

    implemented home page banner

--- src/components/JobHome/JobHome.jsx
+++ src/components/JobHome/JobHome.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import "./JobHome.css";
+import person from "../../../assets/All Images/person.png"
 
 const JobHome = () => {
     return (
-        <div>
-            <h2>This is job home</h2>
+        <div className="banner">
+            <div className="banner-info">
+                <h1>One Step Closer To Your <span>Dream Job</span></h1>
+                <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find
+                    it. Manage all your job application from start to finish.</p>
+                <button>Get Started</button>
+            </div>
+            <img src={person} alt="person image"/>
         </div>
     );
 };
