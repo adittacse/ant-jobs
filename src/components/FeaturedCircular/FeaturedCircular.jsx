71e74b4d4b0d3df23a925a6b5c9bb2787d891c68
Aditta Chakraborty <adittacse@gmail.com>
2023-04-11 01:34:51 +0600

    fetured job element design updated

--- src/components/FeaturedCircular/FeaturedCircular.jsx
+++ src/components/FeaturedCircular/FeaturedCircular.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import "./FeaturedCircular.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot, faCoins } from '@fortawesome/free-solid-svg-icons'
 
 const FeaturedCircular = ({ circular }) => {
     const {id, job_title, company_name, remote_or_onsite, location, salary} = circular;
 
     return (
         <div className="circular">
-            <h3>{job_title}</h3>
-            <h3>{company_name}</h3>
-            <button>{remote_or_onsite}</button>
+            <h3 className="job-title">{job_title}</h3>
+            <h3 className="company-title">{company_name}</h3>
+            <button className="remote_or_onsite" disabled={true}>{remote_or_onsite}</button>
             <div className="location-salary">
-                <p>{location}</p>
-                <p>{salary}</p>
+                <p className="location"><FontAwesomeIcon icon={ faLocationDot } /> {location}</p>
+                <p className="salary"><FontAwesomeIcon icon={ faCoins } /> Salary: {salary}</p>
             </div>
             <button>View Details</button>
         </div>
