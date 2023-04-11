0656781935ac0f869196dc9a90dfa6805b565a9f
Aditta Chakraborty <adittacse@gmail.com>
2023-04-12 00:23:07 +0600

    local storage code file updated

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,18 +1,3 @@
-// use local storage to manage cart data
-const addToDb = id => {
-    let appliedJobs = getAppliedJobs();
-    // add quantity
-    const quantity = appliedJobs[id];
-    if (!quantity) {
-        appliedJobs[id] = 1;
-    }
-    else {
-        const newQuantity = quantity + 1;
-        appliedJobs[id] = newQuantity;
-    }
-    localStorage.setItem('applied-jobs', JSON.stringify(appliedJobs));
-}
-
 const removeFromDb = id => {
     const shoppingCart = getAppliedJobs();
     if (id in shoppingCart) {
@@ -37,7 +22,6 @@ const deleteShoppingCart = () => {
 }
 
 export {
-    addToDb,
     removeFromDb,
     getAppliedJobs,
     deleteShoppingCart
