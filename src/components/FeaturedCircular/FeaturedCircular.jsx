d3d24c4378372c4c9fd645dfc3e98d3562f4552c
Aditta Chakraborty <adittacse@gmail.com>
2023-04-11 17:05:44 +0600

    add slash in route and reload problem solved in jov details page

--- src/components/FeaturedCircular/FeaturedCircular.jsx
+++ src/components/FeaturedCircular/FeaturedCircular.jsx
@@ -2,15 +2,10 @@ import React from 'react';
 import "./FeaturedCircular.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot, faCoins } from '@fortawesome/free-solid-svg-icons'
-import {Link, useNavigate} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const FeaturedCircular = ({ circular }) => {
     const {id, job_title, company_name, remote_or_onsite, location, salary} = circular;
-    const navigate = useNavigate();
-
-    const handleNavigate = () => {
-        navigate(`/circular/${id}`);
-    }
 
     return (
         <div className="circular">
@@ -21,7 +16,6 @@ const FeaturedCircular = ({ circular }) => {
                 <p className="location"><FontAwesomeIcon icon={ faLocationDot } /> {location}</p>
                 <p className="salary"><FontAwesomeIcon icon={ faCoins } /> Salary: {salary}</p>
             </div>
-            {/*<button onClick={handleNavigate}>View Details</button>*/}
             <Link to={`/circular/${id}`}>
                 <button >View Details</button>
             </Link>
